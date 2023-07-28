6093e7817f1eebbafb9c5e0f614782a5807be520
Minty5555 <joshuadaymon2@gmail.com>
2023-07-28 16:32:51 -0400

    new

--- Depend/Dependencies.py
+++ Depend/Dependencies.py
@@ -5,17 +5,18 @@ This Will Install A Text Color Module For Python & pip
 """
 ##This Section Holds Modules##
 import time
+from termcolor import colored
 ##############################
 
 
-print("Installing pip Please Wait...")
+print(colored('Installing pip Please Wait...', 'blue'))
 time.sleep(2)
 import os
 os.system("sudo apt install python3-pip -y")
 
 time.sleep(4)
 
-print("Installing Color Module Please Wait...")
+print(colored('Installing Color Module Please Wait...', 'blue'))
 time.sleep(2)
 import os
 os.system("pip install string-color")
--- ShellCommandExecution.py
+++ ShellCommandExecution.py
@@ -6,25 +6,26 @@ This is my first python script. This Script Greets, Scans, And Enumerates System
 
 
 ##This Section Holds Modules##
-from stringcolor import *
 import time
+from termcolor import colored
 ##############################
 
 
 
+
 #This will locate the Dependency.py script and execute it
-print("Changing Into The Depend Directory. Checking Dependencies...")
+print(colored('Changing Into The Depend Directory. Checking Dependencies...', 'blue'))
 import os
 os.system("cd Depend ; python Dependencies.py")
 
 print("\n")
 
-print(cs("Warming Up Robot Scanner, This Process Wont Take Long.\n","blue"))
+print(colored('Warming Up Robot Scanner, This Process Wont Take Long.\n', 'blue'))
 time.sleep(2)
 
 
 #This will perform a network scan of all live hosts
-print(cs("Robot Initialized Network Scanner", "red"))
+print(colored('Robot Initialized Network Scanner', 'red'))
 import os
 os.system("ping -c 4 8.8.8.8")
 
@@ -33,7 +34,7 @@ print (" \n")
 
 
 #This will find all files starting from the root directory that have the SUID bit set
-print(cs("Robot Activating SUID Enumeration", "red"))
+print(colored('Robot Activating SUID Enumeration', 'red'))
 import os
 os.system("find / -type f -perm -u+s 2>/dev/null ")
 
