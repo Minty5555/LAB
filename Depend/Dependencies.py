c9acd974f9c313df5b740caacb6627f82e4f566f
Minty5555 <joshuadaymon2@gmail.com>
2023-07-28 12:11:32 -0400

    f

---
+++ Depend/Dependencies.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This Will Install A Text Color Module For Python
+"""
+##This Section Holds Modules##
+import time
+##############################
+
+
+
+print("Installing Color Module Please Wait...")
+time.sleep(2)
+import os
+os.system("pip install string-color")
+
