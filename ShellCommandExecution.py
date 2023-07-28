37994a3cff4ff78727ee5e084b8405801fa5f02f
Minty5555 <joshuadaymon2@gmail.com>
2023-07-28 16:15:27 -0400

    tersdt

--- ShellCommandExecution.py
+++ ShellCommandExecution.py
@@ -10,6 +10,8 @@ from stringcolor import *
 import time
 ##############################
 
+
+
 #This will locate the Dependency.py script and execute it
 print("Changing Into The Depend Directory. Checking Dependencies...")
 import os
@@ -20,6 +22,7 @@ print("\n")
 print(cs("Warming Up Robot Scanner, This Process Wont Take Long.\n","blue"))
 time.sleep(2)
 
+
 #This will perform a network scan of all live hosts
 print(cs("Robot Initialized Network Scanner", "red"))
 import os
@@ -27,6 +30,7 @@ os.system("ping -c 4 8.8.8.8")
 
 
 print (" \n")
+
 
 #This will find all files starting from the root directory that have the SUID bit set
 print(cs("Robot Activating SUID Enumeration", "red"))
