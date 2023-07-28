e8e35e3d9e9c0f61e0733c9624b462d6c9a841b0
Minty5555 <joshuadaymon2@gmail.com>
2023-07-27 21:38:39 -0400

    test

--- ShellCommandExecution.py
+++ ShellCommandExecution.py
@@ -5,17 +5,18 @@ This is my first python script. This Script Greets, Scans, And Enumerates System
 """
 from stringcolor import *
 
-print ("Warming Up Robot Scanner, This Process Wont Take Long.\n")
-
+print("Warming Up Robot Scanner, This Process Wont Take Long.\n")
 
 print(cs("Robot Initialized Network Scanner", "red"))
 import os
 os.system("ping -c 4 8.8.8.8")
 
+print (" \n")
+
 print(cs("Robot Activating SUID Enumeration", "red"))
 import os
 os.system("find / -type f -perm -u+s 2>/dev/null ")
- 
+
 
 
 
