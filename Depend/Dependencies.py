87a2daae330ea2ae504834744610f343b031115c
Minty5555 <joshuadaymon2@gmail.com>
2023-07-28 17:02:56 -0400

    trest

--- Depend/Dependencies.py
+++ Depend/Dependencies.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 
 """
-This Will Install pip
+This Will Install pip & print-color
 """
+
 ##This Section Holds Modules##
 import time
-from termcolor import colored
 ##############################
 
-
-print(colored('Installing pip Please Wait...', 'blue'))
+#This Will Install pip
+print("Installing pip Please Wait...")
 time.sleep(2)
 import os
 os.system("sudo apt install python3-pip -y")
+
+print("\n")
+
+#This Will Install Print-Color
+print("Installing Python Colored Please Wait...")
+time.sleep(2)
+import os
+os.system("pip3 install print-color")
 
 
 
--- ShellCommandExecution.py
+++ ShellCommandExecution.py
@@ -7,25 +7,26 @@ This is my first python script. This Script Greets, Scans, And Enumerates System
 
 ##This Section Holds Modules##
 import time
-from termcolor import colored
+from print_color import print
 ##############################
 
+print("Robot Activating SUID Enumeration", color='red')
 
-
+#THESE LINES ARE ON THE BACK BURNER FOR NOW
 
 #This will locate the Dependency.py script and execute it
-print(colored('Changing Into The Depend Directory. Checking Dependencies...', 'blue'))
-import os
-os.system("cd Depend ; python Dependencies.py")
+#print(colored('Changing Into The Depend Directory. Checking Dependencies...', 'blue'))
+#import os
+#os.system("cd Depend ; python Dependencies.py")
 
-print("\n")
+#print("\n")
 
-print(colored('Warming Up Robot Scanner, This Process Wont Take Long.\n', 'blue'))
+print("Warming Up Robot Scanner, This Process Wont Take Long.\n", color='blue')
 time.sleep(2)
 
 
 #This will perform a network scan of all live hosts
-print(colored('Robot Initialized Network Scanner', 'red'))
+print("Robot Initialized Network Scanner", color='red')
 import os
 os.system("ping -c 4 8.8.8.8")
 
@@ -34,7 +35,7 @@ print (" \n")
 
 
 #This will find all files starting from the root directory that have the SUID bit set
-print(colored('Robot Activating SUID Enumeration', 'red'))
+print("Robot Activating SUID Enumeration", color='red')
 import os
 os.system("find / -type f -perm -u+s 2>/dev/null ")
 
