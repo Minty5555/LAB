29dab9763b39daa68a1806a19197f93cd35fa2bf
Minty5555 <joshuadaymon2@gmail.com>
2023-07-30 13:53:29 -0400

    PYCHARM

--- ShellCommandExecution.py
+++ ShellCommandExecution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This is my first python script. This Script Greets, Scans, And Enumerates Systems. I hope you enjoy TEST123
+This is my first python script. This Script Greets, Scans, And Enumerates Systems. I hope you enjoy
 """
 
 
